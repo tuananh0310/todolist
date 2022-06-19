d3228662d8cfe7026a03f9163370af458e013646
tuananh0310 <tuananh0310@gmail.com>
2022-06-19 17:31:03 +0700

    Updates

---
+++ precache-manifest.c11b5c629f15b5b912f67d848f6d5028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eccf72dd06fbc3be37bf",
+    "url": "/tuananh0310/todolist/static/css/main.356c8988.chunk.css"
+  },
+  {
+    "revision": "eccf72dd06fbc3be37bf",
+    "url": "/tuananh0310/todolist/static/js/main.eccf72dd.chunk.js"
+  },
+  {
+    "revision": "a3d08ba48ca3e52f827d",
+    "url": "/tuananh0310/todolist/static/js/1.a3d08ba4.chunk.js"
+  },
+  {
+    "revision": "f80c758418590a53873e",
+    "url": "/tuananh0310/todolist/static/js/runtime~main.f80c7584.js"
+  },
+  {
+    "revision": "9373f2e4f5fce1959e113ebe2dca7cea",
+    "url": "/tuananh0310/todolist/index.html"
+  }
+];
